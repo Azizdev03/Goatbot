ede5f7e904c7d744a4a6d07299b3e03a265c0a44
Azizdev03 <ad3005913@gmail.com>
2025-06-24 23:24:35 +0000

    help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ ➪ 𝘼𝙯𝙞𝙯 🩷🪽 ]";
+const doNotDelete = "[ ➪ Laureine 🩷🪽 ]";
 /** 
 * @author NTKhang
 * @author: do not delete it
